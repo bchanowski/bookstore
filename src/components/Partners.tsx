ed7a90553060096e9f40e389a44bce484c3e6a14
Bartosz Chanowski <bartosz.chanowski@gmail.com>
2025-06-18 15:37:09 +0200

    Worked on the Footer, also small fixes

---
+++ src/components/Partners.tsx
@@ -0,0 +1,21 @@
+import '../styles/Partners.css'
+
+const Partners = () => {
+  return (
+    <div className='partners-div'>
+        <div className='partner-div'>
+            <h3>Logistics Partners: </h3>
+            <p>CityPostOffice</p>
+            <p>DPD</p>
+            <p>DHL</p>
+        </div>
+        <div className='partner-div'>
+            <h3>Payment Partners: </h3>
+            <p>VISA</p>
+            <p>MasterCard</p>
+        </div>
+    </div>
+  );
+};
+
+export default Partners;
