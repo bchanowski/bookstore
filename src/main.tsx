057fdcf7935ba239cf39106fd006407a750963d4
Bartosz Chanowski <bartosz.chanowski@gmail.com>
2025-07-01 13:15:23 +0200

    Added react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainPage from "./pages/MainPage";
 import "./main.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Announcement from "./components/Shared/Announcement";
+import Navbar from "./components/Shared/Navbar";
+import Footer from "./components/Shared/Footer";
+import GoToTopBtn from "./components/Shared/GoToTopBtn";
+import ProductPage from "./pages/ProductPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MainPage />
+    <BrowserRouter>
+      <Announcement />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/product' element={<ProductPage />} />
+      </Routes>
+      <Footer />
+      <GoToTopBtn />
+    </BrowserRouter>
   </StrictMode>
 );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,28 +1,20 @@
-import Announcement from "../components/Shared/Announcement";
-import Footer from "../components/Shared/Footer";
 import MainSlider from "../components/MainPage/MainSlider";
-import Navbar from "../components/Shared/Navbar";
 import ProductsSlider from "../components/MainPage/ProductsSlider";
 import photo1 from "../assets/mainslider/slider1.png";
 import photo2 from "../assets/mainslider/slider2.png";
 import photo3 from "../assets/mainslider/slider3.png";
 import photo4 from "../assets/mainslider/slider4.png";
 import fantasyProducts from "../data/FantasyProducts";
-import GoToTopBtn from "../components/Shared/GoToTopBtn";
 
 const IMAGES = [photo1, photo2, photo3, photo4];
 
 const MainPage = () => {
   return (
     <>
-      <Announcement />
-      <Navbar />
       <MainSlider imagesUrls={IMAGES} />
       <ProductsSlider header="FANTASY" products={fantasyProducts} />
       <ProductsSlider header="ROMANCE" products={fantasyProducts} />
       <ProductsSlider header="EDUCATIONAL" products={fantasyProducts} />
-      <Footer />
-      <GoToTopBtn />
     </>
   );
 };
