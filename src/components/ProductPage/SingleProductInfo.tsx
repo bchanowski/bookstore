af86659c4fc93612faae81c33eaef2e15d53a19c
bchanowski <bartosz.chanowski@gmail.com>
2025-07-06 22:03:01 +0200

    Added add to cart button and changes to basic info and amount counter

--- src/components/ProductPage/SingleProductInfo.tsx
+++ src/components/ProductPage/SingleProductInfo.tsx
@@ -3,6 +3,7 @@ import "../../styles/ProductPage/SingleProductInfo.css";
 import ProductOption from "./ProductOption";
 import PriceGuarantee from "./PriceGuarantee";
 import AmountCounter from "./AmountCounter";
+import { PiShoppingCartSimpleBold } from "react-icons/pi";
 
 type Props = { id: string; product: IProduct };
 
@@ -10,15 +11,24 @@ const SingleProductInfo = ({ id, product }: Props) => {
   const options = ["Soft", "Hard"];
   return (
     <div className="product-page-info-div">
-      <p>#{id}</p>
-      <h2>{product.name}</h2>
-      <p>{product.author}</p>
-      <p>{product.price}</p>
+      <div className="product-basic-info-div">
+        <p className="basic-info-text">#{id}</p>
+        <h2 className="title-text">{product.name}</h2>
+        <p className="basic-info-text">{product.author}</p>
+      </div>
+      <div className="horizontal-line-info" />
+      <div className="price-amount-div">
+        <p>{product.price} €</p>
+        <p className="counter-text">Amount:</p>
+        <AmountCounter max={5} />
+      </div>
+      <div className="horizontal-line-info" />
       <ProductOption name="Cover" options={options} />
-      <p className="product-option-text">Amount:</p>
-      <AmountCounter max={5} />
+      <div className="horizontal-line-info" />
       <PriceGuarantee />
-      <button>add to cart</button>
+      <button className="add-to-cart-btn">
+        <PiShoppingCartSimpleBold className="cart-logo" /> ADD TO CART
+      </button>
       <div>details</div>
       <div>delivery info</div>
     </div>
